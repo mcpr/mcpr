fcbe2ca1b43bef9456b0f2da078d63ede3d7c239
Noah Prail <nprail@filiosoft.com>
2017-06-22 17:22:23 -0400

    Fixed a small bug with urls.

--- server.js
+++ server.js
@@ -74,8 +74,8 @@ router.get(/^(?!.*(docs))/, (req, res) => {
     let url = req.originalUrl;
     let pluginId = false;
     console.log(req.originalUrl);
-    if (url.includes('plugin/')) {
-        pluginId = url.replace('plugin/', '');
+    if (url.includes('/plugin/')) {
+        pluginId = url.replace('/plugin/', '');
     }
     res.render('app', {
         currentUrl: 'https://registry.hexagonminecraft.com' + req.originalUrl,
