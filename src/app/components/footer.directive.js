8d4c165dbf00abe02f565f55a43d889ab71c34d5
Noah Prail <nprail@filiosoft.com>
2017-08-22 08:33:08 -0400

    Lazy load Slack badge!

---
+++ src/app/components/footer.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('app')
+    .directive('footer', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                user: '='
+            },
+            templateUrl: 'layout/footer.html',
+            controller: ['$scope', function ($scope) {
+                [].forEach.call(document.querySelectorAll('img[data-src]'), function (img) {
+                    img.setAttribute('src', img.getAttribute('data-src'));
+                    img.onload = function () {
+                        img.removeAttribute('data-src');
+                    };
+                });
+            }]
+        };
+    });
