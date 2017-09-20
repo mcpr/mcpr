4d3110e0f4a2c40d511f97d1f2d4b6f508fc83ba
Noah Prail <nprail@filiosoft.com>
2017-09-20 12:15:28 -0400

    Login and signup forms now have validation!

--- client/src/app/account/signup/signup.controller.js
+++ client/src/app/account/signup/signup.controller.js
@@ -4,21 +4,23 @@ angular.module('app')
     .controller('SignupCtrl', function ($scope, $http, auth, $state, $window) {
         $scope.user = {
             name: 'Noah Prail',
-            username: 'nprail',
-            email: 'noah@prail.net',
+            username: 'nprail2',
+            email: 'me@nprail.me',
             password: 'fakepwd'
         };
-        $scope.signup = function (user) {
-            auth.signup(user)
-                .then(function (res) {
-                    Materialize.toast('Signup success!', 4000);
-                    $scope.success = true;
-                })
-                .catch(function (err) {
-                    if (err.status === 409) {
-                        return Materialize.toast(err.data.message, 4000);
-                    }
-                    return Materialize.toast(err.statusText, 4000);
-                });
+        $scope.signup = function (user, isValid) {
+            if (isValid) {
+                auth.signup(user)
+                    .then(function (res) {
+                        Materialize.toast('Signup success!', 4000);
+                        $scope.success = true;
+                    })
+                    .catch(function (err) {
+                        if (err.status === 409) {
+                            return Materialize.toast(err.data.message, 4000);
+                        }
+                        return Materialize.toast(err.statusText, 4000);
+                    });
+            }
         }
     });
