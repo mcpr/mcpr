4fa54a8b66b35d5ab59b06847f834c5e9b71ec2d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 02:37:45 +0000

    Deploy mcpr/mcpr to github.com/mcpr/mcpr.git:gh-pages

---
+++ api/endpoints/api_project.js
@@ -0,0 +1,19 @@
+define({
+  "title": "MCPR API Docs",
+  "url": "https://mcpr.io/api/v1",
+  "sampleUrl": "https://mcpr.io/api/v1",
+  "template": {
+    "withCompare": false
+  },
+  "name": "mcpr",
+  "version": "0.0.1",
+  "description": "The ultimate Minecraft plugin registry.",
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-05-27T02:37:29.319Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
